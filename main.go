ad25e5bba5f4a8ddf89de9078dae2bf6f801e8f2
罗宇涛 <1929596302@qq.com>
2023-02-12 21:04:46 +0800

    配置数据库，数据模型

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
-import "ginVueBlog/routes"
+import (
+	"ginVueBlog/model"
+	"ginVueBlog/routes"
+)
 
 func main() {
+	model.InitDb()      //初始化数据库
 	routes.InitRouter() //初始化路由
 
 }
